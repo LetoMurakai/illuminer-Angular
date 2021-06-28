28ee6b31432d433a861fdcdc91e02e08eeedb4af
DavidCostaBKE <daviddacostamariano@outlook.com.br>
2021-06-28 11:58:28 -0300

    componente contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatosComponent } from './contatos/contatos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatosComponent
   ],
   imports: [
     BrowserModule,
