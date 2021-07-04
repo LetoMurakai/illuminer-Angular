0617fa303f4c5ca856f3625c14e096c3ec767a63
DavidCostaBKE <daviddacostamariano@outlook.com.br>
2021-07-04 19:16:11 -0300

    consumindo api na pagina de feed

--- src/app/comentario/comentario.component.ts
+++ src/app/comentario/comentario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Comentario } from '../model/Comentario';
+import { Usuario } from '../model/Usuario';
 
 @Component({
   selector: 'app-comentario',
@@ -6,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comentario.component.css']
 })
 export class ComentarioComponent implements OnInit {
-
+  
+  comentario: Comentario = new Comentario
+  usuario: Usuario = new Usuario
+  fotoUsuario = this.comentario.usuario.foto
+  nomeUsuario = this.comentario.usuario.nome
+  textoDoComentario = this.comentario.texto
+  
+  displayComentarios = "none"
   constructor() { }
 
   ngOnInit(): void {
