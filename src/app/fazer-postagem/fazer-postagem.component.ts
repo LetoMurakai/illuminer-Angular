867508a878a272f78a7331e9a344c313010b3359
DavidCostaBKE <daviddacostamariano@outlook.com.br>
2021-07-04 17:03:50 -0300

    criando o card de fazer a postagem

---
+++ src/app/fazer-postagem/fazer-postagem.component.ts
@@ -0,0 +1,43 @@
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Usuario } from '../model/Usuario';
+import { PostagemService } from '../service/postagem.service';
+
+@Component({
+  selector: 'app-fazer-postagem',
+  templateUrl: './fazer-postagem.component.html',
+  styleUrls: ['./fazer-postagem.component.css']
+})
+export class FazerPostagemComponent implements OnInit {
+
+  postagem: Postagem = new Postagem()
+  listaPostagem: Postagem[]
+  usuario: Usuario = new Usuario()
+  id = environment.id
+  constructor(
+    private router: Router,
+    private PostagemService: PostagemService
+  ) { }
+
+  ngOnInit(){
+    if(environment.token == ''){
+      alert('sua sessão expirou, faça o login novamente')
+      this.router.navigate(['/entrar'])
+    }
+    this.PostagemService.refreshToken()
+  }
+  publicar(){
+    this.PostagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.usuario.id = this.id
+      this.postagem = resp
+      alert('Postagem feita com sucesso')
+      this.postagem = new Postagem()
+    })
+  }
+
+}
