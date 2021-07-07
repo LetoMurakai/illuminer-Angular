3e3bfaf8a6f348768008ccb17ac1241b36b2c589
aline-brilhante <lykarms@gmail.com>
2021-07-07 20:35:14 -0300

    implementação de put, delete e paginação de comentários

--- src/app/model/Comentario.ts
+++ src/app/model/Comentario.ts
@@ -4,7 +4,7 @@ import { Usuario } from "./Usuario"
 export class Comentario{
     public id: number
     public texto: string
-    public data: Date
+    public data: any
     public usuario: Usuario
     public postagem: Postagem[]
 
