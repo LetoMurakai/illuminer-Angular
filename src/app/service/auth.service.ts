8af2fe5773ead6307852fb64466ff92898f2ee7b
DavidCostaBKE <daviddacostamariano@outlook.com.br>
2021-07-09 15:16:58 -0300

    arrumando regra de negocio

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -11,17 +11,28 @@ import { UsuarioLogin } from '../model/UsuarioLogin';
 })
 export class AuthService {
 
+  usuarioLogado: UsuarioLogin = new UsuarioLogin()
   uri = environment.uri
   constructor(
     private http: HttpClient
   ) { }
 
   login(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
+    
     return this.http.post<UsuarioLogin>(`${this.uri}/usuarios/login`, usuarioLogin)
+    
+
   }
 
   cadastro(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>(`${this.uri}/usuarios`, usuario)
-  } 
 
+  }
+  professor(){
+    let professor = true
+    if(environment.tipo != 'professor'){
+      professor = false
+    }
+    return professor
+  }
 }
