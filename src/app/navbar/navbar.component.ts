c5cc61f73863e254d4ba2090cc2ca96de89bf59a
DavidCostaBKE <daviddacostamariano@outlook.com.br>
2021-07-19 09:07:03 -0300

    design botoes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 
 @Component({
@@ -11,12 +11,12 @@ export class NavbarComponent implements OnInit {
   darkMode = environment.darkMode
   textoPesquisa: string
   idUsuarioLogado = environment.id
-
   constructor(
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   pesquisar() {
@@ -46,17 +46,37 @@ export class NavbarComponent implements OnInit {
     this.router.navigate(['/login'])
   }
   trocarCor(){
-    if(this.darkMode == false){
-      environment.darkMode = true
-      this.darkMode = environment.darkMode
-    }
-    else{
-      environment.darkMode = false
-      this.darkMode = environment.darkMode
-    }
+    
+    let id = this.route.snapshot.params['id']
+    console.log(id)
+    if(id == 0 || id == null || id == '' || id == undefined){
+      if(this.darkMode == false){
+        environment.darkMode = true
+        this.darkMode = environment.darkMode
+      }
+      else{
+        environment.darkMode = false
+        this.darkMode = environment.darkMode
+      }
     this.router.navigate(['/pagina-inicio'])
     setTimeout(() => {
       this.router.navigate(['/feed'])
     }, 1);
+  }
+
+    else{
+      if(this.darkMode == false){
+        environment.darkMode = true
+        this.darkMode = environment.darkMode
+      }
+      else{
+        environment.darkMode = false
+        this.darkMode = environment.darkMode
+      }
+      this.router.navigate(['/pagina-inicio'])
+      setTimeout(() => {
+        this.router.navigate([`/perfil/${id}`])
+      }, 1);
+    }
   }
 }
